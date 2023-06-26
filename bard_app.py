5d09a495a3d768111159ed8613825f704f7f5542
Bhavishya Pandit <50860603+bhav09@users.noreply.github.com>
2023-06-26 12:15:23 +0530

    Update bard_app.py

--- bard_app.py
+++ bard_app.py
@@ -1,71 +1,59 @@
-# Importing dependencies
-import asyncio
 import streamlit as st
 from streamlit_chat import message
 from bardapi import Bard
 import json
 
-#Reading the JSON file to fetch credentials
+#Function to generate the output
+def generate_response(prompt):
+    bard = Bard(token=token)
+    response = bard.get_answer(prompt)['content']
+    return response
+
+#Function to receive user queries
+def get_text():
+    input_text = st.text_input("CN Bot", "Hey! ", key='input')
+    return input_text
+
 with open('credentials.json', 'r') as f:
     file = json.load(f)
     token = file['token']
+    
+# TItle of the streamlit app
+st.title('Personal Tutoring Bot!')
 
-# Funtion that generates and returns the response
-def generate_response(prompt):
-    bard = Bard(token=token)
-    response = bard.get_answer(prompt)
-    return response['content']
-
-# Main function
-async def main():
-    st.title("🤖Personal Tutoring!")
-
-    changes = '''
-    <style>
-    [data-testid="stAppViewContainer"]
+# url = 'https://images.unsplash.com/photo-1509773896068-7fd415d91e2e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1738&q=80'
+# data-testid = "stAppViewContainer"
+changes = '''
+<style>
+[data-testid = "stAppViewContainer"]
     {
-    background-image:url('https://images.unsplash.com/photo-1501389683017-9f916b4671b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80');
+    background-image:url('https://images.unsplash.com/photo-1509773896068-7fd415d91e2e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1738&q=80');
     background-size:cover;
     }
-    .st-bx {
-    background-color: rgba(255, 255, 255, 0.05);
-    }
     
-    /* .css-1hynsf2 .esravye2 */
-    
-    html {
-    background: transparent;
-    }
     div.esravye2 > iframe {
         background-color: transparent;
     }
-    </style>
-    '''
+</style>
+'''
+st.markdown(changes, unsafe_allow_html=True)
 
-    # Pushing changes to the UI
-    st.markdown(changes, unsafe_allow_html=True)
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = []
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
 
-    # Get text from the input field
-    def get_text():
-        input_text = st.text_input("You: ", "Hey bot!", key="input")
-        return input_text
 
-    user_input = get_text()
+#Accepting user input
+user_input = get_text()
+if user_input:
 
-    if user_input:
-        output = await asyncio.to_thread(generate_response, user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(output)
+    output = generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
 
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated']) - 1, -1, -1):
-            print(st.session_state["generated"],i)
-            message(st.session_state["generated"][i], key=str(i))
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-
-asyncio.run(main())
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state['generated'][i], key=str(i))
+        message(st.session_state['past'][i], key="user_"+str(i), is_user=True)
